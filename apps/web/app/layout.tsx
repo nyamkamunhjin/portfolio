29cd1b22f8fcef6f50675584633cb174c54ea29f
Munkhjin Nyamdorj <nyamkamunhjin@gmail.com>
2026-04-11 18:35:37 +0800

    feat: initial commit

---
+++ apps/web/app/layout.tsx
@@ -0,0 +1,30 @@
+import { Geist, Geist_Mono, JetBrains_Mono } from "next/font/google"
+
+import "@workspace/ui/globals.css"
+import { ThemeProvider } from "@/components/theme-provider"
+import { cn } from "@workspace/ui/lib/utils";
+
+const fontSans = Geist({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
+
+const jetbrainsMono = JetBrains_Mono({subsets:['latin'],variable:'--font-mono'})
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html
+      lang="en"
+      suppressHydrationWarning
+      className={cn("antialiased", fontSans.variable, "font-mono", jetbrainsMono.variable)}
+    >
+      <body>
+        <ThemeProvider>{children}</ThemeProvider>
+      </body>
+    </html>
+  )
+}
